ef63f69fadaebdf8d86b4befd6b83aacfc9fd5a7
reefman001 <brian.kilburn2@yahoo.com>
2018-09-20 19:57:26 -0400

    still stuck on fitting the model.

--- regression.R
+++ regression.R
@@ -1,5 +1,5 @@
 # Load the raw training data and replace any missing values with NA
-training.data.raw <- read.csv('trainingData.csv', header=T, na.strings=c(""))
+training.data.raw <- read.csv('SpamProcessedData.csv', header=T, na.strings=c(""))
 
 # lets output the number of any missing values
 sapply(training.data.raw, function(x) sum(is.na(x)))
@@ -12,70 +12,38 @@ library(Amelia)
 missmap(training.data.raw, main = "Missing values vs observed")
 
 #subset the data
-data <- subset(training.data.raw, select=c(2,3,4,5))
+data <- subset(training.data.raw, select=c(2,4,5,6,7))
 
 # substitute missing data with average
-data$F2[is.na(data$F2)] <- mean(data$F2, na.rm=T)
+data$F.WordCount[is.na(data$F.WordCount)] <- mean(data$F.WordCount, na.rm=T)
 
 # check factors
 is.factor(data$label)
-is.factor(data$F1)
-is.factor(data$F2)
-is.factor(data$F3)
+is.factor(data$Website)
+is.factor(data$W.count)
+is.factor(data$F.WordCount)
+is.factor(data$Spamword)
 
 # check categorical variable encoding to understand the model
 contrasts(data$label)
-contrasts(data$F3)
+contrasts(data$F.WordCount)
 
 # remove rows in F3 with NAs
-data <- data[!is.na(data$F3),]
+data <- data[!is.na(data$F.WordCount),]
 rownames(data) <- NULL
 
-#process testing data
-
-# Load the raw training data and replace any missing values with NA
-test.data.raw <- read.csv('testData.csv', header=T, na.strings=c(""))
-
-# lets output the number of any missing values
-sapply(test.data.raw, function(x) sum(is.na(x)))
-
-# check how many different values for each feature
-sapply(test.data.raw, function(x) length(unique(x)))
-
-# visualize missing data
-library(Amelia)
-missmap(test.data.raw, main = "Missing values vs observed")
-
-#subset the data
-testdata <- subset(test.data.raw, select=c(2,3,4,5))
-
-# substitute missing data with average
-testdata$F2[is.na(testdata$F2)] <- mean(testdata$F2, na.rm=T)
-
-# check factors
-is.factor(testdata$label)
-is.factor(testdata$F1)
-is.factor(testdata$F2)
-is.factor(testdata$F3)
-
-# check categorical variable encoding to understand the model
-contrasts(testdata$label)
-contrasts(testdata$F3)
-
-# remove rows in F3 with NAs
-testdata <- testdata[!is.na(testdata$F3),]
-rownames(testdata) <- NULL
-
-#training and testing data is already split via python
-train = training.data.raw
-test = test.data.raw
-
+#split the data for training and testing set
+train <- data[1: 2000,]
+test <- data[2001:4001,]
+test
 # Model fitting
-model <- glm(label ~., family=subset(link='logit'), data=train)
+model <- glm(label ~., family=binomial(link='logit'), data=train)
 model
+
 #measure predictability ### errors
 #####################################
-predict(model, newdata=subset(test,select=c(2,3,4,5)), type='response')
+fitted.results <- predict(model, newdata=subset(test,select=c(2,3,4,5)), type='response')
+fitted.results
 fitted.results <- ifelse(fitted.results > 0.5, 1, 0)
 fitted.results
 library(ROCR)
