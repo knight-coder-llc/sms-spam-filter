593c2b082d70b0240dda26513783d1950dc125f4
EKUStudent <EKUStudent@HPMXL4192PT9.student.eku.edu>
2018-09-20 10:10:35 -0400

    refined code to produce only one file and added new features

--- detection_filter.py
+++ detection_filter.py
@@ -6,10 +6,11 @@ sources: https://towardsdatascience.com/spam-classifier-in-python-from-scratch-2
 """
 
 import nltk as lang
+#use this if you do not have nltk module
 #lang.download('all')
 import numpy as np
 import matplotlib.pyplot as plot
-from wordcloud import WordCloud
+#from wordcloud import WordCloud
 from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords
 from collections import Counter
@@ -21,7 +22,7 @@ import re
 import string
 
 #create spam word cloud
-def createWordCloud(data):
+'''def createWordCloud(data):
     s_words = ' '.join(list(data[data['label'] == 1]['SMS Message']))
     word_count = WordCloud(width = 512, height = 512).generate(s_words)
     plot.figure(figsize = (10, 8), facecolor = 'k')
@@ -39,12 +40,20 @@ def createWordCloud(data):
     plot.axis('off')
     plot.tight_layout(pad = 0)
     print('HAM')
-    plot.show()
+    plot.show()'''
 
 # measure the length of the message
 def messageLength(data):
     messageLen = len(data)
     return messageLen
+
+#get upper case word count
+def checkUpper(data):
+    count = 0
+    for index, value in enumerate(data):
+        if value.isupper():
+            count += 1
+    return count
 
 #check if message has a website url
 def hasWebsite(data):
@@ -55,21 +64,33 @@ def hasWebsite(data):
     return 0
 
 #check for the most frequent words
-def mostFrequentWords(data):   
+def mostFrequentWords(data, word=True):   
     counts = Counter(data)
-    item = counts.most_common(1)
-    for index, value in enumerate(item):
-        item = value[1]
-    #print(item)    
-    return item
     
-def preProcessMessage(data,stop_words = True, stemm = True, lower = True, grams = 2):
+    if(word):
+        item = counts.most_common(1)
+        for index, value in enumerate(item):
+            item = value[1]    
+        return item
+    else:
+        #return string literal word
+        item = counts.most_common(1)
+        for index, value in enumerate(item):
+            item = value[0]
+        return item
+    
+def preProcessMessage(data,stop_words = True, stemm = True, lower = True, grams = 2, tokenize = True, punctuation = True):
     #set the words to lowercase, this helps us to not deal with free or FREE as two different words 
+    #print(data)
     if(lower):
         data = data.str.lower()
         #remove punctuations
+    if(punctuation):
         data = data.str.translate(str.maketrans("","",'|-[]_.:;,!?&()''""\\'))
+        print(data)
         #tokenize the data
+        
+    if(tokenize):
         tokens = [lang.word_tokenize(token) for token in data]
 
     #use n-grams to improve accuracy
@@ -89,11 +110,15 @@ def preProcessMessage(data,stop_words = True, stemm = True, lower = True, grams 
         stemmer = SnowballStemmer('english')
         for index, value in enumerate(tokens):
             tokens[index] = [stemmer.stem(token) for token in value]
-    return tokens
+            
+    if tokenize:
+        return tokens
+    return data
 
 def main():
-    trainfeatureFrame = []
-    testfeatureFrame = []
+    '''trainfeatureFrame = []
+    testfeatureFrame = []'''
+    featureFrame = []
     #set column width to display data
     pd.set_option('display.max_colwidth', 100)
     #read file and create the dataframe
@@ -102,7 +127,7 @@ def main():
     df = pd.DataFrame(data)
     
     #split the data into test and training set
-    trainingSet, testSet = list(), list()
+    '''trainingSet, testSet = list(), list()
     for i in range(df['SMS Message'].shape[0]):
         if np.random.uniform(0, 1) < 0.50:
             trainingSet += [i]
@@ -115,29 +140,73 @@ def main():
     train.drop(['index'], axis = 1, inplace=True)
     
     test.reset_index(inplace=True)
-    test.drop(['index'], axis = 1, inplace=True)
+    test.drop(['index'], axis = 1, inplace=True)'''
     
     #wordcloud = WordCloud().generate_from_frequencies(train['SMS Message'])
     #print(wordcloud)
     
     #process messages before createing a word cloud for optimization
-    train['SMS Message'] = preProcessMessage(train['SMS Message'])
-    test['SMS Message'] = preProcessMessage(test['SMS Message'])
+    #train['SMS Message'] = preProcessMessage(train['SMS Message'])
     
-    for i in range(len(train)):
-      trainfeatureFrame.append(hasWebsite(train['SMS Message'][i]))
-      
-    for i in range(len(test)): 
-      testfeatureFrame.append(hasWebsite(test['SMS Message'][i]))
+    # we do not have to split the data until tested in r
+    #df['SMS Message'] = preProcessMessage(df['SMS Message'])
+    
+    #test['SMS Message'] = preProcessMessage(test['SMS Message'])
+    
+    #need to tokenize before searching a url and convert to lowercase
+    df['SMS Message'] = preProcessMessage(df['SMS Message'], True, False, True, 2, True, True)
+    
+    for i in range(len(df)):
+      featureFrame.append(hasWebsite(df['SMS Message'][i]))
+     
+    #append feature to the data frame
+    df2 = pd.DataFrame(featureFrame)
+    df['Website'] = df2
+    
+    featureFrame = []
+    
+    #get most frequent word count
+    for i in range(len(df)):
+        featureFrame.append(mostFrequentWords(df['SMS Message'][i]))
+    
+    #append feature to the data frame
+    df2 = pd.DataFrame(featureFrame)
+    df['F-WordCount'] = df2
+    
+    featureFrame = []
+    
+    #get most frequent word
+    for i in range(len(df)):
+        featureFrame.append(mostFrequentWords(df['SMS Message'][i], False))
+    print(featureFrame)
+    
+    df2 = pd.DataFrame(featureFrame)
+    df['F-word'] = df2
+    
+    print(df)
+    
+    # prep message for most frequent word count and extraction, lowercase, tokenizing, stemming, punctuation and n-gram is completed.
+    #df['SMS Message'] = preProcessMessage(df['SMS Message'], True, False, False, 1, False, False)
+    
+    #print(df['Website'])
+    #preProcessMessage(data,stop_words = True, stemm = True, lower = True, grams = 2, tokenize = True, punctuation = True):
+    #df['SMS Message'] = preProcessMessage(df['SMS Message'], True, False, True, 2, True, True)
+    #print(df['SMS Message'])
+    # frequent word count
+    '''for i in range(len(df)):
+        featureFrame.append(mostFrequentWords(df['SMS Message'][i]))
+    print(featureFrame)
     #append feature to the data frame
     df2 = pd.DataFrame(trainfeatureFrame)
-    train['F1'] = df2
+    train['F3'] = df2
+    
+    # frequent word
+    for i in range(len(df)):
+        featureFrame.append(mostFrequentWords(df['SMS Message'][i], False))
+    print(featureFrame)
     
     df2 = pd.DataFrame(testfeatureFrame)
-    test['F1'] = df2
-    #print(train)
-    trainfeatureFrame = []
-    testfeatureFrame = []
+    test['F3'] = df2
     
     for i in range(len(train)):
       trainfeatureFrame.append(messageLength(train['SMS Message'][i]))
@@ -155,17 +224,7 @@ def main():
     trainfeatureFrame = []
     testfeatureFrame = []
     
-    for i in range(len(train)):
-        trainfeatureFrame.append(mostFrequentWords(train['SMS Message'][i]))
-        
-    for i in range(len(test)):
-        testfeatureFrame.append(mostFrequentWords(test['SMS Message'][i]))
-    #append feature to the data frame
-    df2 = pd.DataFrame(trainfeatureFrame)
-    train['F3'] = df2
     
-    df2 = pd.DataFrame(testfeatureFrame)
-    test['F3'] = df2
     
     #remove the sms message column
     train = train.drop(['SMS Message'], axis = 1)
@@ -192,6 +251,7 @@ def main():
     
     #create and export the processed dataset?
     train.to_csv('./trainingData.csv', encoding='utf-8-sig')
-    test.to_csv('./testData.csv', encoding='utf-8-sig')
+    test.to_csv('./testData.csv', encoding='utf-8-sig')'''
+    df.to_csv('./SpamProcessedData.csv', encoding='utf-8-sig')
     print('done')
 main()
